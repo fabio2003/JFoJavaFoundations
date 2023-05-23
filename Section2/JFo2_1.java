52dd97afeea05643e0ca6d04d62d944ea59181fd
Fábio Augusto Vitório <44537162+fabio2003@users.noreply.github.com>
2023-05-23 11:57:36 -0300

    Add files via upload

---
+++ Section2/JFo2_1.java
@@ -0,0 +1,18 @@
+/* Nome do Aluno: Fábio Vitório
+* RA: 323135349
+* Nome do Programa: JFo Section 2
+* Data: 22/05/23
+*/
+
+class JFo2_1 {
+  public static void main(String[] args) {
+    System.out.println("  ####  ");
+    System.out.println(" #    # ");
+    System.out.println("# #  # #");
+    System.out.println("#      #");
+    System.out.println("# #  # #");
+    System.out.println("#  ##  #");
+    System.out.println(" #    # ");
+    System.out.println("  ####  ");
+  }
+}
